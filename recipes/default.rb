8f83e8eafd21a17dbd1250f943e90de30fbd034c
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-11-01 16:17:10 -0500

    add updates for operating systems

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,6 +11,13 @@ service 'sshd' do
 end
 
 if node['platform_family'] == "debian"
+  execute 'Update Debian based operating systems' do
+    command 'apt-get update -y'
+  end
+
+  execute 'Install git and wget on Debian' do
+    command 'apt-get install git wget -y'
+  end
 
   user 'cloud_user' do
     home '/home/cloud_user'
@@ -35,12 +42,28 @@ if node['platform_family'] == "debian"
 end
 
 if node['platform_family'] == "rhel"
+  execute 'Update RHEL based operatings systems' do
+    command 'yum update -y'
+  end
+
+  execute 'Install git and wget on RHEL' do
+    command 'yum install git wget -y'
+  end
+
   execute 'add cloud_user to sudoers' do
     command '/bin/echo \'cloud_user  ALL=(ALL)  NOPASSWD: ALL\' >> /etc/sudoers'
   end
 end
 
 if node['platform_family'] == "amazon"
+  execute 'Update Amazon based operatings systems' do
+    command 'yum update -y'
+  end
+
+  execute 'Install git and wget on Amazon' do
+    command 'yum install git wget -y'
+  end
+
   user 'cloud_user' do
     home '/home/cloud_user'
     manage_home true
