1be50b015382eb219ed2ca815dd79fb13dec6384
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-11-13 14:53:02 -0600

    install rvm for centos

--- recipes/default.rb
+++ recipes/default.rb
@@ -51,7 +51,18 @@ if node['platform_family'] == "rhel"
   end
 
   execute 'Install git and wget on RHEL' do
-    command 'yum install git wget ruby -y'
+    command 'yum install git wget -y'
+  end
+
+  bash 'install rvm' do
+    code <<-EOH
+    gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+    curl -sSL https://get.rvm.io | sudo bash -s stable
+    usermod -a -G rvm `whoami`
+    if sudo grep -q secure_path /etc/sudoers; then sudo sh -c "echo export rvmsudo_secure_path=1 >> /etc/profile.d/rvm_secure_path.sh" && echo Environment variable installed; fi
+    source /etc/profile.d/rvm.sh
+    rvm install 2.2.4
+    EOH
   end
 
   execute 'add cloud_user to sudoers' do
