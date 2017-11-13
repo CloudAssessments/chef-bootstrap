07bd63f2a7a205bc3eee0773d5b9058b1e987839
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-11-13 14:04:21 -0600

    install rvm

--- recipes/default.rb
+++ recipes/default.rb
@@ -65,7 +65,31 @@ if node['platform_family'] == "amazon"
   end
 
   execute 'Install git and wget on Amazon' do
-    command 'yum install git wget ruby -y'
+    command 'yum install git wget gpg gcc gcc-c++ make -y'
+  end
+
+  execute 'Install Key' do
+    command 'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3'
+  end
+
+  execute 'Install install rvm' do
+    command 'curl -sSL https://get.rvm.io | sudo bash -s stable'
+  end
+
+  execute 'Add user rvm' do
+    command 'usermod -a -G rvm `whoami`'
+  end
+
+  execute 'Install install rvm' do
+    command 'if sudo grep -q secure_path /etc/sudoers; then sudo sh -c "echo export rvmsudo_secure_path=1 >> /etc/profile.d/rvm_secure_path.sh" && echo Environment variable installed; fi'
+  end
+
+  execute 'Source rvm' do
+    command 'source /etc/profile.d/rvm.sh'
+  end
+
+  execute 'Install ruby 2.2.4' do
+    command 'rvm install 2.2.4'
   end
 
   user 'cloud_user' do
