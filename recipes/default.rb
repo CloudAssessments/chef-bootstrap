ff837015a7daa3479de554f2cdaba13f52cd5476
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-11-16 16:54:44 -0600

    remove rvm for now

--- recipes/default.rb
+++ recipes/default.rb
@@ -80,16 +80,16 @@ if node['platform_family'] == "amazon"
     command 'yum install git wget gpg gcc gcc-c++ make -y'
   end
 
-  bash 'install rvm' do
-    code <<-EOH
-    gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-    curl -sSL https://get.rvm.io | sudo bash -s stable
-    usermod -a -G rvm `whoami`
-    if sudo grep -q secure_path /etc/sudoers; then sudo sh -c "echo export rvmsudo_secure_path=1 >> /etc/profile.d/rvm_secure_path.sh" && echo Environment variable installed; fi
-    source /etc/profile.d/rvm.sh
-    rvm install 2.2.4
-    EOH
-  end
+  #bash 'install rvm' do
+  #  code <<-EOH
+  #  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+  #  curl -sSL https://get.rvm.io | sudo bash -s stable
+  #  usermod -a -G rvm `whoami`
+  #  if sudo grep -q secure_path /etc/sudoers; then sudo sh -c "echo export rvmsudo_secure_path=1 >> /etc/profile.d/rvm_secure_path.sh" && echo Environment variable installed; fi
+  #  source /etc/profile.d/rvm.sh
+  #  rvm install 2.2.4
+  #  EOH
+  #end
 
   #execute 'Install Key' do
   #  command 'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3'
